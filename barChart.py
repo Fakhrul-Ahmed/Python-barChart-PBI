2acfa9d71ca6a3f6d7170732658de81738154368
Fakhrul-Ahmed <160753477+Fakhrul-Ahmed@users.noreply.github.com>
2024-03-06 23:03:20 +0100

    Update barChart.py

--- barChart.py
+++ barChart.py
@@ -8,12 +8,6 @@
 # mydata = pd.read_excel("file location/ file path")
 # mydata = pd.read_csv("file location/ file path")
 
-#(Personal learning only: how to aggragate summary
-        # I need a summary from mydata, and I am using the aggregate function,
-        # and setting the labels in the chart by index function (.reset_index())
-# summary = pd.pivot_table(mydata, index = 'Year', aggfunc='sum').reset_index()  
-# Still learning: found problem here!!!)
-
 import matplotlib as mb
 import matplotlib.pyplot as plt
 mb.rcParams['font.size']=19.0
